56e704fff75447f83bd81089e00a9873eb2dc85b
Malte Baumann <malte.baumann@9elements.com>
2017-03-16 12:11:56 +0100

    Ionic app can open PESDK

--- PESDKDemo/src/pages/home/home.ts
+++ PESDKDemo/src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+declare var PESDK;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -12,4 +14,16 @@ export class HomePage {
     
   }
 
+  onButtonClick (event) {
+    console.log("Opening PESDK...");
+    PESDK.present(
+      function(result){
+        alert('PESDK result: ' + JSON.stringify(result));
+      },
+      function(error){
+        alert('PESDK error: ' + error);
+      },
+      {sourceType: 1}
+  );
+  }
 }
